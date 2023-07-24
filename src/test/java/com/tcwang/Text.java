2cadb2eceea22a212127220e76d0241fae8e9f90
kakajn <2248263651@qq.com>
2023-07-24 18:03:03 +0800

    test

---
+++ src/test/java/com/tcwang/Text.java
@@ -0,0 +1,11 @@
+package com.tcwang;
+
+/**
+ * @author: JKin
+ * @createTime: 2023/7/24 18:02
+ * @fileDescriptions: You had better view this comment before you use it.
+ * This is a new test file.
+ */
+
+public class Text {
+}
